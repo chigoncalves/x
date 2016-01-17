404331bda32328436f69afa226bc2334c8b8a04d
Edelcides Gonçalves <eatg75@gmail.com>
2016-01-17 00:32:48 +0000

    Initial commit

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+/**
+ * Professor Farnsworth - The bad new everyone is: the beast reproduces asexually, a single bone vampire left alone with some quiet music and some bone vampire porn could produce dozens of deadly off springs.
+ * Dr. Zoidberg - That lucky bastard! I say kill it.
+ * Fry - It loves me and I love it.
+ * Dr. Zoidberg - Kill them both.
+*/
