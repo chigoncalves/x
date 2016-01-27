055807802d15c636014ea7e96762813224f4755c
Edelcides Gonçalves <eatg75@gmail.com>
2016-01-27 09:17:32 +0000

    Changed dir struct.

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-define('APP_STARTED', 'true');
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-ini_set('short_open_tag', true);
+define ('APP_STARTED', 'true');
+error_reporting (E_ALL);
+ini_set ('display_errors', true);
+ini_set ('short_open_tag', true);
 
 require_once 'lib/db-info.php';
 
-var_dump($db_credentials);
 ?>
---
+++ lib/db-info.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined ("APP_STARTED")) {
+  return;
+}
+
+$db = array (
+  'host' => 'localhost',
+  'usr' => 'db-consumer',
+  'passwd' => 'AbCd3F!',
+);
+?>
